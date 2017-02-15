0107e33004c696855e13ccd4a72acc62c4cdad2c
wb-xwb199998 <wb-xwb199998@alibaba-inc.com>
2017-02-15 15:04:05 +0800

    webgl

--- WebGL/test/lib/getwebgl.js
+++ WebGL/test/lib/getwebgl.js
@@ -1,6 +1,6 @@
 var webGLUtil = (function(){
     function getWebGLContext(canvas){
-        var name = ['webgl', 'exprimental-webgl', 'webkit-3d', 'moz-webgl'];
+        var name = ['webgl', 'experimental-webgl', 'webkit-3d', 'moz-webgl'];
         var context = null;
         for(var i = 0, len = name.length; i < len; i++){
             try{
